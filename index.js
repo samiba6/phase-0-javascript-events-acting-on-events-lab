427292c2297412e9f134b64d0945556d4da97e4d
Sami <s.babouche@esi-sba.dz>
2023-09-13 02:25:02 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+    else{
+        moveDodgerRight()
+    }
+  });
+
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight(){
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+
+    
+  }
